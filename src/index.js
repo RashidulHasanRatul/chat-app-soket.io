dc804e054ccea2480e028f46375ecaeda0fa6111
Rashidul Hasan <rhratul12@gmail.com>
2022-10-08 13:36:41 +0600

    serve html home page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
+app.use(express.static(publicDirectoryPath));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send();
 });
 
 app.listen(port, () => {
-  console.log(` App listening at http://localhost:${port}`);
+  console.log(` Server is up on port ${port}`);
 });
