f90593c5f6311d42c23662bea54f4c5bbe555ecf
Rashidul Hasan <rhratul12@gmail.com>
2022-10-09 01:26:20 +0600

    User added notification and Geolocation share feature added

--- src/index.js
+++ src/index.js
@@ -11,9 +11,25 @@ const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 
-
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("New WebSocket connection");
+  socket.emit("message", "Welcome New User ");
+  socket.broadcast.emit("message", "A new user has joined");
+
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
+  });
+
+  socket.on("sendLocation", (coords) => {
+    io.emit(
+      "message",
+      `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+    );
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left");
+  });
 });
 
 server.listen(port, () => {
